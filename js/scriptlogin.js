99d6adfd447e4b9af0ff4fc69a482cbcc8657fb4
diegoalor <diegoalor@upeu.edu.pe>
2020-05-12 17:03:31 -0500

    Ejercicio

--- js/scriptlogin.js
+++ js/scriptlogin.js
@@ -1,4 +1,41 @@
 class login{
-    constructor(usuario,password,iniciar);
-    this.usuario = prompt()
+    constructor(usuario,password,is){
+    this.usuario = ;
+    this.password = ;
+    }
+    /*
+    usuario(){
+        if (this.usuario != "diego") {
+            return alert("Usuario Invalido");
+        } else {
+            return alert("Bienvenido a la Deep Web");
+        }
+    }
+    password(){
+        if (this.password != "alor") {
+            return alert("Password Incorrecto");
+        } else {
+            return alert("Bienvenido a la Deep Web");
+        }
+    }
+    */
+   iniciar(){
+       if (this.usuario=="diego") {
+           if (this.password=="alor") {
+                return alert("Bienvenido a la Deep Web")
+           } else {
+                return alert("Password Incorrecto");
+           }
+       } else {
+            return alert("Usuario Incorrecto");
+       }
+   }
+}
+
+function iniciarS() {
+    var sesion = new login(
+        document.getElementById("user").value,
+        document.getElementById("pass").value);
+
+        sesion.document.getElementById("boton").value=iniciar();
 }
